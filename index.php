428d9b304102f360c2622aabe209f685583ecd51
Caio Lima <ticaiolima@gmail.com>
2012-09-27 23:31:34 -0300

    teste

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
-<?php
 
-echo "12345";
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bootstrap 101 Template</title>
+<!-- Bootstrap -->
 
-?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap-responsive.css" rel="stylesheet">
+<link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<body>
+
+
+
+	<div class="row-fluid">
+		<table class="table table-bordered">
+
+			<tr class="success">
+				<td>teste 1</td>
+				<td>teste 2</td>
+				<td>teste 3</td>
+			</tr>
+			<tr>
+				<td>teste 1</td>
+				<td>teste 2</td>
+				<td>teste 3</td>
+			</tr>
+			<tr>
+				<td>teste 1</td>
+				<td>teste 2</td>
+				<td>teste 3</td>
+			</tr>
+
+		</table>
+	</div>
+
+</body>
+</html>
