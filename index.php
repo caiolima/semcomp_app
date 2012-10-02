00d9b8df1f7c47c269731c0a11ce4dce57e6d848
Caio Lima <ticaiolima@gmail.com>
2012-10-02 11:17:25 -0300

    Img dias

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-  
+<?php 
+	ini_set('default_charset','UTF-8');
+?>  
 <!DOCTYPE html>
 <html>
 <head>
@@ -25,6 +27,30 @@
 
 	<div class="well well-small marge_15" align="center" style="color: white;">
 		O que esta acontecendo?
+	</div>
+
+	<div class="well well-small marge_15" align="center">
+
+		<div class="row-fluid">
+			<div class="span6">
+				<a href="show_eventos.php?action=categoria&idcategoria=2"><img width="100px" height="100px" src="img/dia_1.png" class="img-rounded"></a>
+			</div>
+			<div class="span6">
+				<a href="show_eventos.php?action=categoria&idcategoria=3"><img width="100px" height="100px" src="img/dia_2.png" class="img-rounded"></a>
+			</div>
+		</div>
+		<div class="row-fluid" style="margin-top: 20px">
+			<div class="span6">
+				<a href="show_eventos.php?action=categoria&idcategoria=4"><img width="100px" height="100px" src="img/dia_3.png" class="img-rounded"></a>
+			</div>
+			<div class="span6">
+				<a href="show_eventos.php?action=categoria&idcategoria=5"><img width="100px" height="100px" src="img/dia_4.png" class="img-rounded"></a>
+			</div>
+		</div>
+	</div>
+	
+	<div class="well well-small marge_15" align="center" style="color: white;">
+		Em cada espaço
 	</div>
 
 	<div class="well well-small marge_15">
